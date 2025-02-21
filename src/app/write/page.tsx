314821291d7a3a2fbccbd2bae9e7847fea4b2d69
Gurpreet <gs109690@gmail.com>
2025-02-21 22:38:59 +0530

    update input

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react'
 import { useReadContract } from 'wagmi'
 import Messages from '@/components/messages'
 import { chatroomAbi } from '@/utils/chatroomAbi'
@@ -6,11 +7,12 @@ import { chatroomAbi } from '@/utils/chatroomAbi'
 const CONTRACT_ADDRESS = '0x515Bcc986E17FDbd38FBb3585f12D1D4b53ecE66'
 
 export default function Write() {
+  const [message, setMessage] = useState('')
+
   const {
     data: allMessages,
     isFetching: isFetchingMessages,
     refetch: refetchMessages,
-    isError,
     error,
   } = useReadContract({
     abi: chatroomAbi,
@@ -27,7 +29,12 @@ export default function Write() {
       <h3 className="text-4xl opacity-75">Write to smart contracts</h3>
       <div className="flex flex-col items-center gap-4">
         <div className="flex flex-col items-start gap-10 rounded-lg border border-white/10 bg-[#2d2d2d] px-4 py-4 lg:min-w-[600px]">
-          <input className="w-full bg-transparent text-xl focus:outline-none"></input>
+          <input
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            className="w-full bg-transparent text-xl focus:outline-none"
+            placeholder="Type your message..."
+          />
         </div>
         <button className="mx-auto max-w-[200px] rounded-lg border border-white/55 px-8 py-2 duration-200 hover:border-white/85 hover:bg-zinc-800/40">
           Send Message
